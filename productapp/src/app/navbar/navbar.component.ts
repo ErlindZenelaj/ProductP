458150a798d76d69523810437b4ffc2a54f35739
Erlind <81335694+ErlindZenelaj@users.noreply.github.com>
2023-10-13 14:54:58 +0200

    small changes

--- productapp/src/app/navbar/navbar.component.ts
+++ productapp/src/app/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-navbar',
@@ -23,6 +24,13 @@ export class NavbarComponent implements OnInit {
   }
 
   logout() {
+    Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'You have been logged out!',
+      showConfirmButton: false,
+      timer: 1500
+    }) 
     this.authService.logout();
     this.router.navigate(['login']);
   }
